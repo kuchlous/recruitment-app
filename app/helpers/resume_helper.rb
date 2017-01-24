402ca1b93b74e543256048c458cb08ad6eba4e5c
bhargavah <hemant.bhargava7@gmail.com>
2017-01-24 15:53:49 +0530

    1. Sorted footer links. 2. Go to bottom links. This was needed because many times comments/messages become very big and it took time to scroll to the end part of the page. 3. Displaying feedback summary. 4. Displaying feedback average.

--- app/helpers/resume_helper.rb
+++ app/helpers/resume_helper.rb
@@ -84,4 +84,25 @@ module ResumeHelper
      return "Orange"
     end
   end
+
+  def get_rating(feedback)
+    return feedback.rating
+  end
+
+  def get_feedback_rating_average(feedbacks)
+    feedback_avg = 0;
+    feedback = Hash.new
+    feedback["Poor"] = 1
+    feedback["Fair"] = 2
+    feedback["Good"] = 3
+    feedback["Very Good"] = 4
+    feedback["Excellant"] = 5
+    feedbacks.each do |f|
+      logger.info(f.rating)
+      feedback_avg += feedback[f.rating]
+      logger.info(feedback[f.rating])
+    end
+    logger.info(feedback_avg)
+    return (feedbacks.size == 0) ? 0 : (feedback_avg.to_f/(feedbacks.size)).round(2)
+  end
 end
