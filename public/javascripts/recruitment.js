4067faca98e724c40730ea140659b798dced3442
anwesha debnath <engganwesha@gmail.com>
2020-06-08 13:19:38 +0530

    added loader for ajax call under resume action

--- public/javascripts/recruitment.js
+++ public/javascripts/recruitment.js
@@ -1360,6 +1360,8 @@ function createAjaxRequest(cur_element, req_match_id, value, resume_id, req_matc
   Event.observe(element, "click",
     function(element)
     {
+      document.getElementById("loader").style.display="flex";
+  
       // If action is shortlist then we have to pass forward_id as well as req_ids
       // So i merged forward_id with "Shortlist" earlier. Now i am extracting forward_id
       if ( value.match("Shortlist") )
@@ -1389,12 +1391,14 @@ function createAjaxRequest(cur_element, req_match_id, value, resume_id, req_matc
           parameters: 'resume[comment]=' + encodeURIComponent($F('comment_textarea')) + params,
           onSuccess: function(transport)
           {
+            document.getElementById("loader").style.display="none";
             value = findProperValueToBeDisplayed(value);
             deleteAndCreateTDAfterAction(elements[2], value);
             changeCurrentRowColor(elements[3]);
           },
           onFailure: function(transport)
           {
+            document.getElementById("loader").style.display="none";
             alert("Server was down while performing this action. Please contact administrators.");
           }
         });
