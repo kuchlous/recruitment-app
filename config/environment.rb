c60cbcdda5e8e868b704f6ec3c1fe39eab73b9d4
anwesha debnath <engganwesha@gmail.com>
2020-06-10 16:55:55 +0530

    fixed precompile issue in css

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
 # Initialize the Rails application.
 Rails.application.initialize!
 
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
