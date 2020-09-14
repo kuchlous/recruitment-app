219bbdbb84d950b991313260e995b5fc2c677d0c
For deploying apps <apps@apps.mirafra.com>
2020-09-14 16:58:40 +0530

    Fix for next in search page. Ensure that the next gets host name and add GET path to config/routes as the next link is a GET, not a POST

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   match 'requirements/close_requirement', to: 'requirements#close_requirement' , via: :post
   resources :requirements
   match 'home/search', to: 'home#search'                                       , via: :post
+  match 'home/search', to: 'home#search'                                       , via: :get
   match 'home/show_summary_per_interviewer', to:'home#show_summary_per_interviewer'                                       , via: :get
   match 'home/show_summary_per_recruiter', to: 'home#show_summary_per_recruiter'                                     , via: :get
 
