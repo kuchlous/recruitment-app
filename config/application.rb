fb2b157835e3bece655499519c5bd4b7b19e7e9b
Alok Kuchlous <alokk@mirafra.com>
2025-07-14 14:44:39 +0530

    Load modules from lib directory as TextExtractor lives there now

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Recruit5
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     Warning[:deprecated] = false
+    
+    # Load custom lib files
+    config.autoload_paths += %W(#{config.root}/lib)
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
